1fb664bcec8da2b6c1be54a46d412bd14f87c11a
dongyu.zym <dongyu.zym@alibaba-inc.com>
2021-12-16 17:05:12 +0800

    [47] AC

--- src/leetcode/editor/en/[47]_permutations_i_i.rs
+++ src/leetcode/editor/en/[47]_permutations_i_i.rs
@@ -40,31 +40,45 @@ use std::mem::swap;
 
 impl Solution {
     pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut map = HashMap::new();
+        let mut ret = Vec::new();
         let mut nums = nums;
-        Self::backtrack(&mut nums, 0, &mut map);
-        map.values().cloned().collect()
-        // map.into_iter().
+
+        let mut stack = Vec::new();
+        stack.resize(nums.len(), -100);
+        // let mut stack = Vec::with_capacity(nums.len());
+        let mut visited = Vec::new();
+        visited.resize(nums.len(), false);
+
+        nums.sort_unstable();
+        Self::backtrack(&mut nums, 0, &mut ret, &mut stack, &mut visited);
+        ret
     }
 
-    fn backtrack(nums: &mut Vec<i32>, level: usize, ret: &mut HashMap<String, Vec<i32>>) {
-        let current_num_key = Self::toString(nums);
-        if level == nums.len() - 1 {
-            ret.entry(current_num_key).or_insert(nums.clone());
+    fn backtrack(
+        nums: &mut Vec<i32>,
+        level: usize,
+        ret: &mut Vec<Vec<i32>>,
+        stack: &mut Vec<i32>,
+        visited: &mut Vec<bool>,
+    ) {
+        let len = nums.len();
+        if level == len {
+            ret.push(stack.clone());
+            return;
         }
-        for i in level..nums.len() {
-            nums.swap(level, i);
-            Self::backtrack(nums, level + 1, ret);
-            nums.swap(level, i);
-        }
-    }
 
-    fn toString(nums: &mut Vec<i32>) -> String {
-        let mut t = Vec::new();
-        for num in nums {
-            t.append(&mut num.to_be_bytes().to_vec());
+        for i in 0..len {
+            if visited[i] || (i > 0 && nums[i - 1] == nums[i] && !visited[i - 1]) {
+                continue;
+            }
+            visited[i] = true;
+            stack[level] = nums[i];
+            // stack.push(nums[i]);
+            Self::backtrack(nums, level + 1, ret, stack, visited);
+            visited[i] = false;
+            stack[level] = -100;
+            // stack.pop();
         }
-        unsafe { String::from_utf8_unchecked(t) }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
@@ -81,5 +95,16 @@ mod tests {
         assert_eq!(ret, vec![vec![1, 1, 2],
                              vec![1, 2, 1],
                              vec![2, 1, 1]]);
+    }
+    #[test]
+    fn test_2() {
+        // test code here
+        let ret = Solution::permute_unique(vec![2,2,1,1]);
+        assert_eq!(ret, vec![vec![1,1,2,2],
+                             vec![1,2,1,2],
+                             vec![1,2,2,1],
+                             vec![2,1,1,2],
+                             vec![2,1,2,1],
+                             vec![2,2,1,1]]);
     }
 }
